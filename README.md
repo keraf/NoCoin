b4b88d1dbb4187a80db7ccb86d19572fe13a644f
Rafael Keramidas <rafael@keramid.as>
2017-09-16 21:47:41 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,19 @@
+# No Coin
+No coin is a tiny browser extension aiming to block coin miners such as Coinhive.
+
+
+You can grab the extension from the Opera Web Store: https://addons.opera.com/en/extensions/details/no-coin/?display=en
+
+### Why?
+Even though I think using coin mining in browser to monetize content is a great idea, abusing it is not. Some websites are running it during the entire browsing session which results in high consumption of your computers resources. I do believe that using it occasionally such as for the proof of work of a captcha is OK. But for an entire browsing session, the user should have the choice to opt-in which is the aim of this extension.
+
+### How does it work?
+The extension is simply blocking a list of blacklisted domains in *blacklist.txt*. Clicking on the icon will display you a button to pause/unpause No Coin. If you are aware of any scripts or services that provide coin mining the browser, please submit a PR.
+
+## Contribute
+Contributions are welcome! Don't hesitate to submit bug fixes, improvements and new features. 
+
+Regarding new features, please have a look at the issues first. If a feature you whish to work on is not listed in here, you might want to add an issue first before starting to work on a PR.
+
+
+*Made by Arthur Geron (hellguy [at] protonmail [dot] com - [@arthurgeron](https://www.twitter.com/arthurgeron) - [arthur.geron](https://arthurgeron.azurewebsites.net/))*
---
+++ js/background.js
@@ -0,0 +1,132 @@
+/**
+ * No Coin - Stop coin miners in your browser
+ **
+ * @author      Arthur Geron <arthur.geron>
+ * @version     0.4
+ * @license     GNULGPL3.0
+ * @source      https://github.com/arthurgeron/NoCoin
+ */
+
+// Config
+const defaultConfig = {
+    toggle: true,
+};
+
+const localConfig = JSON.parse(localStorage.getItem('config'));
+let config = {
+    ...defaultConfig,
+    ...localConfig,
+};
+
+// Functions
+const saveConfig = () => {
+    localStorage.setItem('config', JSON.stringify(config));
+};
+
+const changeToggleIcon = (isEnabled) => {
+    chrome.browserAction.setIcon({
+        path: `img/${isEnabled ? 'logo' : 'logo_disabled'}.png`,
+    });
+};
+
+const getDomain = (url) => {
+    let match = url.match(/:\/\/(.[^/]+)/gi);
+    if (!!match) {
+        return match[0];
+    } else {
+        return '';
+    }
+};
+
+const getTimestamp = () => {
+    return Math.floor(Date.now() / 1000);
+};
+
+const isDomainWhitelisted = (domain) => {
+    if (!domain) return false;
+
+    const domainInfo = config.whitelist.find(w => w.domain === domain);
+
+    if (domainInfo) {
+        if (domainInfo.expiration !== 0 && domainInfo.expiration <= getTimestamp()) {
+            removeDomainFromWhitelist(domain);
+
+            return false;
+        }
+
+        return true;
+    }
+
+    return false;
+};
+
+const addDomainToWhitelist = (domain, time) => {
+    if (!domain) return;
+
+    // Make sure the domain is not already whitelisted before adding it
+    if (!isDomainWhitelisted(domain)) {
+        config.whitelist = [
+            ...config.whitelist,
+            {
+                domain: domain,
+                expiration: time === 0 ? 0 : getTimestamp() + (time * 60),
+            },
+        ];
+        saveConfig();
+    }
+};
+
+const removeDomainFromWhitelist = (domain) => {
+    if (!domain) return;
+
+    config.whitelist = config.whitelist.filter(w => w.domain !== domain);
+    saveConfig();
+}
+
+// Main
+if (!config.toggle) {
+    changeToggleIcon(false);
+}
+
+const blacklist = chrome.runtime.getURL("blacklist.txt");
+fetch(blacklist)
+    .then(resp => {
+        resp.text()
+            .then(text => {
+                const blacklistedUrls = text.split('\r\n');
+                
+                chrome.webRequest.onBeforeRequest.addListener(details => {
+                    if (!config.toggle) {
+                        return { cancel: false };
+                    }
+            
+                    return { cancel: true };
+                }, { 
+                    urls: blacklistedUrls
+                }, ['blocking']);
+            })
+            .catch(err => {
+                // TODO: Handle this
+                console.log(err);
+            });
+    })
+    .catch(err => {
+        // TODO: Handle this
+        console.log(err);
+    });
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    switch (message.type) {
+        case 'GET_STATE':
+            sendResponse({
+                toggle: config.toggle,
+            });
+            break;
+        case 'TOGGLE':
+            config.toggle = !config.toggle;
+            saveConfig();
+            changeToggleIcon(config.toggle);
+            sendResponse(config.toggle);
+            break;
+    }
+});
