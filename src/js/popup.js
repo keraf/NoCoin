7e93c18f9c8797a533f645caa70f3db2fe2c1596
suhail sullad <suhail-sullad@gmail.com>
2017-09-21 09:35:39 +0530

    Revert "Spelling changes and ability to direclty download blacklist.txt file from github repo"
    This reverts commit db8b929c7205009e45544ad9ef0cc719f8a77228.

--- src/js/popup.js
+++ src/js/popup.js
@@ -18,7 +18,7 @@ $(() => {
             $('.toggle').addClass('green').removeClass('red');
         }
 
-        $('.toggle').text(isEnabled ? 'Pause No Coin' : 'Resume No Coin');
+        $('.toggle').text(isEnabled ? 'Pause No Coin' : 'Unpause No Coin');
     }
 
     const showWhitelistButtons = (isVisible) => {
